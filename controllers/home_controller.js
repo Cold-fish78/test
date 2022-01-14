c7eeadb08c8f32e9d24e6cd706ad7ff0c5b65b1c
coldfish <pubg69089@gmail.com>
2022-01-14 23:47:41 +0530

    started

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = function(req,res){
+    return res.render('home',{title : "this is title of the page"});
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 8008;
+app.use(express.urlencoded({extended : true}));
+
+
+// for ejs no
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('./assets'));
+//  for routes;
+// app.use('/',('./routes'));
+app.use('/',require('./routes/index'));
+
+app.listen(port, function (err) {
+    if(err){
+        console.log("error in running the server");
+
+    }else{
+        console.log("server running at port" + port);
+    }
+})
