b5e889988dd868bd430e7b2bb99b3ef6f7cb66b0
coldfish <pubg69089@gmail.com>
2022-01-30 16:37:02 +0530

    updated title

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,5 +1,5 @@
 module.exports.blogpost = function(req,res){
-    res.render('blogpost',{title : 'blogpost'})
+    res.render('blogpost',{title : 'AWS not accepting debit/credit card [solved]'})
 }
 module.exports.search = function(req,res){
     res.render('search',{title : 'search results'});
